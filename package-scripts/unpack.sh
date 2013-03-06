5cb60cafad9124882e648f4cf4437c790085634f
Russell Francis <russell.francis@metro-six.com>
2013-03-06 16:15:01 -0500

    BugzID: 856, Modified the oopool to work with LibreOffice 4.0.1.2.  This includes changes to the DocumentConverter.py and pyjavaproperties.py file since we now have to use python 3.3 which ships with LibreOffice in order to convert documents.

--- package-scripts/unpack.sh
+++ package-scripts/unpack.sh
@@ -4,6 +4,11 @@
 # libre office you can then package up the created opt/libreoffice4
 # directory for use with the deployment scripts as part of oopool.
 #
+# Also note that the libreoffice-java package also needs to be manually
+# constructed from the necessary .jar files.  It should be noted that
+# those files should have their Sealed: true status removed from the
+# MANIFEST.MF file if such an entry exists in those jar files.
+#
 FILES=`ls *.rpm`
 for FILE in $FILES; do
     rpm2cpio "$FILE" | cpio -idmv
